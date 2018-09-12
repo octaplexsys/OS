fd2078d6429f1d70a63bc4bdaa82a0a0203e1aa5
Temur <tim_1998@bk.ru>
2018-09-12 23:56:58 +0300

    Assignment Lab. Week 04

--- week4/ex4.c
+++ week4/ex4.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define BUFFER_SIZE 64
+#define BG_JOB "&"
+#define QUIT "q"
+#define NEW_CMD "> "
 
 int main() {
 	printf("Input q to quit the program\n");
 	while(1) {
-		char str[100];
-		printf("> ");
-		gets(str);
-		if(!strcmp(str, "q")) 
+		char str[BUFFER_SIZE];
+		char str2[BUFFER_SIZE];
+		char cmd[BUFFER_SIZE];
+		char *pointer;
+		printf(NEW_CMD);
+		fgets(str, BUFFER_SIZE, stdin);
+		if(!strcmp(str, QUIT)) 
 			break;
-		system(str);
+		strcpy(str2, str);
+		pointer = strtok(str, " ");
+		if(pointer != NULL){
+		    strcpy(cmd, pointer);
+		}
+		if(strchr(str2, BG_JOB)) {
+			int pid = fork();
+			if(!pid) {
+				if (execvp(cmd, str) < 0) {     
+					printf("*** ERROR: exec failed\n");
+					exit(1);
+			    	}
+			}
+		}
+		else {
+			system(str2);
+		}
 	}
 	return 0;
 }
